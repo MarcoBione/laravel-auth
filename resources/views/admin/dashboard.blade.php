4540bdf39644c5597cb059c36f55b780051a2105
MarcoBione <marcobione.work@gmail.com>
2023-06-06 15:15:38 +0200

    Remove default pages

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,50 +2,6 @@
 
 @section('content')
     <div class="container">
-        <h2 class="fs-4 text-secondary my-4">
-            {{ __('Welcome') }} {{ Auth::user()->name }}
-        </h2>
-        <div class="row justify-content-center">
-            <div class="col">
-                <div class="card mb-3">
-                    <div class="card-header">{{ __('Comandi') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        <div class="d-flex justify-content-between">
-
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="card mb-3">
-                    <div class="card-header">{{ __('Dati') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        <div class="d-flex justify-content-between">
-                            <div class="border border-1">
-                                left
-                            </div>
-                            <div class="border border-1">
-                                right
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 @endsection
