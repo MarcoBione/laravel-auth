9f5eb57914bab578c44d1809a841c3e583006113
MarcoBione <marcobione.work@gmail.com>
2023-06-09 18:16:44 +0200

    Add config file + project seeder

---
+++ config/projects.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'projects' => [
+        [
+            "name" => "Laravel-primi-passi",
+            "image" => "#",
+            "description" => "esercizio di approccio a laravel"
+        ],
+        [
+            "name" => "Laravel-comic",
+            "image" => "#",
+            "description" => "primo esercizio con laravel"
+        ],
+
+    ]
+];
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Ignition\Contracts\ConfigManager;
 
 class ProjectSeeder extends Seeder
 {
@@ -14,6 +16,16 @@ class ProjectSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $projects = config('projects');
+
+        foreach($projects as $project){
+            $newProject = new Project();
+
+            $newProject->name = $project['name'];
+            $newProject->image = $project['image'];
+            $newProject->description = $project['description'];
+
+            $newProject->save();
+        }
     }
 }
